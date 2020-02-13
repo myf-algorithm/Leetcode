af15d38b11d928eed60f3b442f1d0053d26a2583
myf-algorithm <1332046108@qq.com>
2020-02-13 19:11:02 +0800

    第5题，注释完成

--- Swordoffer/5.用两个栈实现队列.py
+++ Swordoffer/5.用两个栈实现队列.py
@@ -7,13 +7,19 @@ class Solution:
         self.stack2 = []
 
     def push(self, node):
+        # 将新元素压入堆栈stack1
         self.stack1.append(node)
 
     def pop(self):
-        # stack2非空
+        # stack2非空，输出stack2的栈顶元素
         if len(self.stack2):
             return self.stack2.pop()
-        # stack2是空的，把stack1中的元素全部压入stack2中
+        # stack2是空的，把stack1中的栈顶元素依次压入stack2中
+        # 如此处理之后
+        # stack1中的栈顶元素被压入stack2的栈底
+        # stack1中的栈底元素被压入stack2的栈顶
+        # stack2下次进行pop时，输出stack1中的栈底元素
+        # 如此实现了stack1中的元素，按照先入先出的顺序输出
         while self.stack1:
             self.stack2.append(self.stack1.pop())
         # 判断stack2是否是空的，非空则输出，空则return
