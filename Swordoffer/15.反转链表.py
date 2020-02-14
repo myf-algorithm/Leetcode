8393209474b307e76f8ebee80e7044e9f62d965b
myf-algorithm <1332046108@qq.com>
2020-02-14 15:55:56 +0800

    15题

--- Swordoffer/15.反转链表.py
+++ Swordoffer/15.反转链表.py
@@ -12,12 +12,17 @@ class Solution:
     def ReverseList(self, pHead):
         head = None
         while pHead:
-            tmp = ListNode(pHead.val)
-            tmp.next = head
-            head = tmp
-            pHead = pHead.next
+            tmp = ListNode(pHead.val)  # 使用当前位置的值创建一个新节点
+            tmp.next = head  # 令新节点指向head，此时方向已经反过来了
+            head = tmp  # 更新head为新节点
+            pHead = pHead.next  # 移动到下一个位置
         return head
 
+    # 递归首先找到新链表的头部节点，然后递归栈返回，层层反转
+    # 首先找到新链表的头结点（即遍历到原链表的最后一个节点返回最后节点）
+    # 执行函数体后续代码，将原链表中的尾节点指向原尾节点的前置节点
+    # 前置节点的指针指向None（防止出现死循环）
+    # 返回新链表的头部节点至上一层函数，重复以上操作
     def ReverseList_r(self, pHead):
         if pHead == None or pHead.next == None:
             return pHead
@@ -26,7 +31,7 @@ class Solution:
         pHead.next = None
         return p
 
-    def travel_list(self, pHead):
+    def travel_list(self, pHead):   # 遍历链表，打印节点的值
         while pHead:
             print(pHead.val, end=' ')
             pHead = pHead.next
@@ -43,4 +48,4 @@ if __name__ == '__main__':
     n2.next = n3
     n3.next = n4
     S = Solution()
-    S.travel_list(S.ReverseList(n0))
+    S.travel_list(S.ReverseList_r(n0))
