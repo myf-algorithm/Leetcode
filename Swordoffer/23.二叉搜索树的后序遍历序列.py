02b75003be38d7e834193eace525338d025bd428
myf-algorithm <1332046108@qq.com>
2020-02-15 16:44:58 +0800

    23题

--- Swordoffer/23.二叉搜索树的后序遍历序列.py
+++ Swordoffer/23.二叉搜索树的后序遍历序列.py
@@ -5,18 +5,19 @@
 
 class Solution:
     def VerifySquenceOfBST(self, sequence):
-        if len(sequence) == 0:
+        if len(sequence) == 0:  # 如果序列长度为0，返回False
             return False
         if len(sequence) == 1 or len(sequence) == 2:
-            return True
+            return True  # 如果序列长度为1或2，返回True
         else:
-            root = sequence.pop(-1)
+            root = sequence.pop(-1)  # 弹出序列的最后一个节点为根节点
             sign = 0
             for i in range(len(sequence)):
-                if sequence[i] > root:
-                    sign = 1
-                if (sign == 1) and (sequence[i] < root):
+                if sequence[i] > root:  # 已经找到比根节点大的元素
+                    sign = 1  # 将标志向量置1
+                if (sign == 1) and (sequence[i] < root):    # 再次找到比根节点小的元素
                     return False
+            # 递归验证左子树和右子树的后序遍历序列
             return self.VerifySquenceOfBST(sequence[:i]) and self.VerifySquenceOfBST(sequence[i:])
 
 
