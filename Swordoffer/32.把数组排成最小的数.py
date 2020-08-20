cd99b868103812591e7b4f20eca1fadc763b8062
马一凡 <mayifan@yiwise.com>
2020-08-20 14:29:21 +0800

    sort

--- Swordoffer/32.把数组排成最小的数.py
+++ Swordoffer/32.把数组排成最小的数.py
@@ -6,6 +6,15 @@
 # 比较两个字符串s1, s2大小的时候，先将它们拼接起来，比较s1+s2,和s2+s1那个大
 # 如果s1+s2大，那说明s2应该放前面，所以按这个规则，s2就应该排在s1前面
 
+# list.sort()：list会发生改变，只能在列表上使用
+# new_list = sorted(list)：list不会发生改变，返回结果给new_list，可在列表、字典上使用
+# sort和sorted的key参数：指定一个函数，此函数只有一个参数，返回一个值进行比较
+# 在python3中，提供将cmp转化为key的函数
+# cmp参数指定的函数需要2个参数，用来进行元素间的比较
+# 返回负数表示小于
+# 0表示等于
+# 正数表示大于
+
 
 class Solution:
     def PrintMinNumber(self, numbers):
@@ -20,3 +29,19 @@ class Solution:
 if __name__ == '__main__':
     S = Solution()
     print(S.PrintMinNumber([3, 32, 321]))
+
+    # sorted中key参数的使用方法
+    student1 = {
+        "age": 152
+    }
+    student2 = {
+        "age": 132
+    }
+    student3 = {
+        "age": 122
+    }
+    student4 = {
+        "age": 1223
+    }
+    student_list = [student1, student2, student3, student4]
+    print(sorted(student_list, key=lambda student: student["age"]))
