7a89edf91ec764911e699d3371839ca39a7ef443
马一凡 <mayifan@yiwise.com>
2020-08-20 10:19:31 +0800

    类似于快排

--- Swordoffer/13.调整数组顺序使奇数位于偶数前面.py
+++ Swordoffer/13.调整数组顺序使奇数位于偶数前面.py
@@ -53,11 +53,15 @@ class Solution3:
         head = 0
         tail = n - 1
         while head < tail:
+            # 头指针遇到了第一个偶数
             while array[head] % 2 != 0:
                 head += 1
+            # 尾指针遇到了第一个奇数
             while array[tail] % 2 == 0:
                 tail -= 1
+            # 将奇偶数位置互换
             array[head], array[tail] = array[tail], array[head]
+            # 奇偶指针向中间移动
             head += 1
             tail -= 1
 
