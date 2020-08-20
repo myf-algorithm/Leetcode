83e7bd21a8d1fd6bd406082544bbeec456a6cd99
马一凡 <mayifan@yiwise.com>
2020-08-20 20:20:24 +0800

    修改

--- Swordoffer/51.构建乘积数组.py
+++ Swordoffer/51.构建乘积数组.py
@@ -7,12 +7,17 @@
 class Solution:
     def multiply(self, A):
         size = len(A)
+        # 初始化
         B = [1] * size
+
+        # 计算前边的部分
         # 下三角: B[i]=A[0]A[1]A[2]..A[i−1] = B[i−1]A[i−1]
         for i in range(1, size):
             B[i] = B[i - 1] * A[i - 1]
-        tmp = 1
+
+        # 计算后面的部分
         # 上三角（从最后往前）tmp=A[−1]A[−2]A[−3]...
+        tmp = 1
         for i in range(size - 2, -1, -1):
             tmp = tmp * A[i + 1]
             B[i] = B[i] * tmp
