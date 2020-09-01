dc5c53f770421e2ba6abcb50d0d2a818424c80db
myf-algorithm <1332046108@qq.com>
2020-09-01 14:29:30 +0800

    修改

--- Leetcode/88.合并两个有序数组.py
+++ Leetcode/88.合并两个有序数组.py
@@ -44,6 +44,23 @@ class Solution(object):
             nums1[p1 + p2:] = nums2[p2:]
 
 
+class Solution1:
+    def merge(self, A, m, B, n):
+        if not A:
+            return B
+        if not B:
+            return A
+        while m > 0 and n > 0:
+            if A[m - 1] > B[n - 1]:
+                A[m + n - 1] = A[m - 1]
+                m -= 1
+            else:
+                A[m + n - 1] = B[n - 1]
+                n -= 1
+        if n > 0:
+            A[:n] = B[:n]
+        return A
+
 
 if __name__ == '__main__':
     S = Solution()
