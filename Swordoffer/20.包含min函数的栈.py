6effca6648875f193b67b7113cdcdaf7c3bcd116
myf-algorithm <1332046108@qq.com>
2020-09-01 16:07:12 +0800

    修改

--- Swordoffer/20.包含min函数的栈.py
+++ Swordoffer/20.包含min函数的栈.py
@@ -27,6 +27,47 @@ class Solution:
         return self.minstack[-1]
 
 
+# 两个栈的实现方法
+class Solution1:
+    def __init__(self):
+        self.stackData = []
+        self.stackMin = []
+
+    def push(self, num):
+        if not self.stackMin:
+            self.stackMin.append(num)
+        elif num <= self.getMin():
+            self.stackMin.append(num)
+        else:
+            self.stackMin.append(self.stackMin[-1])
+        self.stackData.append(num)
+
+    def pop(self):
+        if not self.stackData:
+            return "Your stack is empty."
+        self.stackMin.pop()
+        return self.stackData.pop()
+
+    def getMin(self):
+        if not self.stackMin:
+            return "Your stack is empty."
+        return self.stackMin[-1]
+
+    def getMinStack(self, op):
+        # write code here
+        res = []
+        if op == []:
+            return res
+        for i in op:
+            if i[0] == 1:
+                self.push(i[1])
+            elif i[0] == 2:
+                self.pop()
+            elif i[0] == 3:
+                res.append(self.getMin())
+        return res
+
+
 if __name__ == '__main__':
     S = Solution()
     S.push(2)
