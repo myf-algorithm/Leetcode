4f8864e313cb056a9f82c8bb521af876fa5c50f2
myf-algorithm <1332046108@qq.com>
2020-09-02 11:06:20 +0800

    修改

--- Leetcode/4.寻找两个有序数组的中位数.py
+++ Leetcode/4.寻找两个有序数组的中位数.py
@@ -48,6 +48,40 @@ class Solution:
         raise ValueError('传入无效的参数，输入的数组不是有序数组，算法失效')
 
 
+# find median in two sorted array
+# 在两个长度相等的排序数组中找中位数
+# @param arr1 int整型一维数组 the array1
+# @param arr2 int整型一维数组 the array2
+# @return int整型
+
+class Solution1:
+    def findMedianinTwoSortedAray(self, arr1, arr2):
+        if not arr1 or not arr2: return -1
+        l1, r1, l2, r2 = 0, len(arr1) - 1, 0, len(arr2) - 1
+        m1 = (len(arr1) - 1) // 2
+        m2 = m1
+        l = 0
+        while l1 < r1:
+            m1 = (l1 + r1) // 2
+            m2 = (l2 + r2) // 2
+            l = r1 - l1 + 1
+            if arr1[m1] > arr2[m2]:
+                r1 = m1
+                if l % 2 == 0:
+                    l2 = m2 + 1
+                else:
+                    l2 = m2
+            elif arr1[m1] < arr2[m2]:
+                if l % 2 == 0:
+                    l1 = m1 + 1
+                else:
+                    l1 = m1
+                r2 = m2
+            else:
+                return arr2[m2]
+        return min(arr1[l1], arr2[l2])
+
+
 if __name__ == '__main__':
     s = Solution()
     nums1, nums2 = [1, 3, 4, 5], [2]
