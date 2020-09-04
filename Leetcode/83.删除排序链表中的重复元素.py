7ad7a0d648f1f8deb5a02ebad461932490cac86d
myf-algorithm <1332046108@qq.com>
2020-09-04 19:32:17 +0800

    修改

---
+++ Leetcode/83.删除排序链表中的重复元素.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = head
+        while cur:
+            if cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy.next
