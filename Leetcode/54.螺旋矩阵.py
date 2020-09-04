e6f52b7f84361bcb1dcb565055afdd8609fa6a8b
myf-algorithm <1332046108@qq.com>
2020-09-04 11:20:56 +0800

    修改

--- Leetcode/54.螺旋矩阵.py
+++ Leetcode/54.螺旋矩阵.py
@@ -6,21 +6,21 @@ class Solution(object):
         """
         if not matrix:
             return []
-        R, C = len(matrix), len(matrix[0])
-        seen = [[False] * C for _ in matrix]
-        ans = []
-        dr = [0, 1, 0, -1]
+        R, C = len(matrix), len(matrix[0])  # 矩阵的行数，列数
+        seen = [[False] * C for _ in matrix]  # 初始化集合，记录已经走过的坐标
+        ans = []  # 初始化，存储遍历后的矩阵元素
+        dr = [0, 1, 0, -1]  # 方向是：右，下，左，上
         dc = [1, 0, -1, 0]
-        r = c = di = 0
+        r = c = di = 0  # 矩阵元素，方向变量初始化
         for _ in range(R * C):
-            ans.append(matrix[r][c])
-            seen[r][c] = True
-            cr, cc = r + dr[di], c + dc[di]
-            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+            ans.append(matrix[r][c])  # 存储遍历矩阵过的元素
+            seen[r][c] = True  # 存储遍历过的坐标
+            cr, cc = r + dr[di], c + dc[di]  # 先记录下一步坐标，用于判断下一步怎么走
+            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:  # 判断坐标是否需变向，且没有遍历过
                 r, c = cr, cc
             else:
-                di = (di + 1) % 4
-                r, c = r + dr[di], c + dc[di]
+                di = (di + 1) % 4  # 改变方向，右下左上为一圈，防止方向坐标越界
+                r, c = r + dr[di], c + dc[di]  # 下一步坐标
         return ans
 
 
