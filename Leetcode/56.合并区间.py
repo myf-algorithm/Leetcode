0e0de6b9de992005746a6c42c433cd41f1bfda3a
myf-algorithm <1332046108@qq.com>
2020-09-06 19:42:50 +0800

    修改

--- Leetcode/56.合并区间.py
+++ Leetcode/56.合并区间.py
@@ -14,6 +14,7 @@ class Solution(object):
             # 下一个左边的值小于等于当前右边的值，合并
             while i < n - 1 and intervals[i + 1][0] <= right:
                 i += 1
+                # 更新最大的右边的值
                 right = max(intervals[i][1], right)
             res.append([left, right])
             i += 1
