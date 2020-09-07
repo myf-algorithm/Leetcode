2f50f7031b7d1324b836f44bebdfbc814e78cc20
myf-algorithm <1332046108@qq.com>
2020-09-07 15:41:14 +0800

    修改

--- Swordoffer/24.二叉树中和为某一值的路径.py
+++ Swordoffer/24.二叉树中和为某一值的路径.py
@@ -4,6 +4,8 @@
 # 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
 # (注意: 在返回值的list中，数组长度大的数组靠前)
 
+from typing import List
+
 
 class TreeNode:
     def __init__(self, x):
@@ -13,21 +15,42 @@ class TreeNode:
 
 
 class Solution:
-    # 返回二维列表，内部每个列表表示找到的路径
-    def FindPath(self, root, expectNumber):
-        result = []
-        if not root:
-            return result
-        # 如果只有根节点或者找到叶子节点，我们就把其值返回
-        if not root.left and not root.right and root.val == expectNumber:
-            return [[root.val]]
-        # 如果不是叶子节点，我们分别对根节点的左子树、右子树进行递归，注意修改变量:
-        else:
-            left = self.FindPath(root.left, expectNumber - root.val)
-            right = self.FindPath(root.right, expectNumber - root.val)
-            for item in left + right:
-                result.append([root.val] + item)
-            return result
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res, path = [], []
+
+        def dfs(root, sum):
+            if not root:
+                return
+            path.append(root.val)
+            sum -= root.val
+            if sum == 0 and not root.left and not root.right:
+                res.append(list(path))
+            dfs(root.left, sum)
+            dfs(root.right, sum)
+            path.pop()
+
+        dfs(root, sum)
+        return res
+
+
+class Solution1:
+    def pathSum(self, root, sum):
+        # write code here
+        res = []
+        path = []
+        self.dfs(root, res, sum, path)
+        return res
+
+    def dfs(self, root, res, sum, path):
+        if root is None:
+            return
+        if not root.left and not root.right and sum == root.val:
+            res.append(path + [root.val])
+            return
+        path.append(root.val)
+        self.dfs(root.left, res, sum - root.val, path)
+        self.dfs(root.right, res, sum - root.val, path)
+        path.pop()
 
 
 if __name__ == '__main__':
