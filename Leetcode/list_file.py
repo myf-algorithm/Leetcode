b9674c8adc30782aa4c34cb5d58c69fea30210b2
myf-algorithm <1332046108@qq.com>
2020-09-08 16:30:35 +0800

    修改

---
+++ Leetcode/list_file.py
@@ -0,0 +1,19 @@
+# encoding=utf-8
+
+import os
+import operator
+
+def file_name(file_dir):
+    for root, dirs, files in os.walk(file_dir):
+        print(files)  # 当前路径下所有非目录子文件
+        files_dic = {}
+        for i in files[:-2]:
+            i = i.split('.')
+            files_dic[int(i[0])] = i[1:]
+        print(files_dic)
+        files_dic = dict(sorted(files_dic.items(), key=lambda x: x[0]))
+        with open('a.text', 'w') as f:
+            for key, value in files_dic.items():
+                f.write('| ' + str(key) + '.' + str(value[0]) + ' |' +  '\n')
+
+file_name('./')
