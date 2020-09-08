183853b4b549ea991b407109b2104c39e553a977
myf-algorithm <1332046108@qq.com>
2020-09-08 14:53:37 +0800

    修改

--- Swordoffer/67.把数字翻译成字符串.py
+++ Swordoffer/67.把数字翻译成字符串.py
@@ -19,7 +19,6 @@ class Solution_newcoder:
         dp = [0 for _ in range(n + 1)]
         dp[0] = 1
         for i in range(1, n + 1):
-
             if i == 1:
                 c = nums[i - 1]
                 code1 = int(c)
