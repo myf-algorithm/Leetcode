b389461be1455a4998b5e2cd21a5fe1a7e2b12a6
myf-algorithm <1332046108@qq.com>
2020-09-10 15:20:40 +0800

    修改

--- Leetcode/122.买卖股票的最佳时机II.py
+++ Leetcode/122.买卖股票的最佳时机II.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         profit = 0
         for i in range(1, len(prices)):
-            tmp = prices[i] - prices[i -1]
+            tmp = prices[i] - prices[i - 1]
             if tmp > 0:
                 profit += tmp
         return profit
