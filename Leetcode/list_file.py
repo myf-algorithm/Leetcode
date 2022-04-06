8394cb90e19ac4b1b87eb02d9d01c954fdf5ff70
myf-algorithm <1332046108@qq.com>
2022-04-06 19:42:48 +0800

    20220406

--- Leetcode/list_file.py
+++ Leetcode/list_file.py
@@ -7,10 +7,12 @@ import operator
 def file_name(file_dir):
     for root, dirs, files in os.walk(file_dir):
         print(files)  # 当前路径下所有非目录子文件
+        print(len(files))
         files_dic = {}
-        for i in files[:-2]:
-            i = i.split('.')
-            files_dic[int(i[0])] = i[1:]
+        for i in files:
+            if i != 'list_file.py' and i != 'a.text':
+                i = i.split('.')
+                files_dic[int(i[0])] = i[1:]
         print(files_dic)
         files_dic = dict(sorted(files_dic.items(), key=lambda x: x[0]))
         with open('a.text', 'w') as f:
