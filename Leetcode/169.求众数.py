168f2113de8c2664ba7fb30e157aae7759642cf9
myf-algorithm <1332046108@qq.com>
2022-04-06 14:00:06 +0800

    169

--- Leetcode/169.求众数.py
+++ Leetcode/169.求众数.py
@@ -32,3 +32,17 @@ class Solution(object):
         for i in set1:
             if dic.get(i) > (len(nums) // 2):
                 return i
+
+    def majorityElement(self, nums: List[int]) -> int:
+        vote = 0
+        res = 0
+        for i in range(len(nums)):
+            if vote == 0:
+                res = nums[i]
+                vote += 1
+            else:
+                if nums[i] == res:
+                    vote += 1
+                else:
+                    vote -= 1
+        return res
