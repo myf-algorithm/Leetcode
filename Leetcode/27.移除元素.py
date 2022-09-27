40950daaa7ed5a075b1e4bb49513c0f8a2958d16
myf-algorithm <1332046108@qq.com>
2022-09-27 15:20:50 +0800

    927

---
+++ Leetcode/27.移除元素.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            else:
+                fast += 1
+        return slow
