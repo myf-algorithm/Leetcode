2077be9fa9b2bd936827685b1c5e34c324b66ff0
myf-algorithm <1332046108@qq.com>
2022-11-29 14:15:24 +0800

    98

--- Leetcode/98.验证二叉搜索树.py
+++ Leetcode/98.验证二叉搜索树.py
@@ -101,6 +101,23 @@ class Solution(object):
         return True
 
 
+class Solution1:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def traverse(root, min_node, max_node):
+            # base case
+            if root is None:
+                return True
+            # 如果 root.val 不满足 max 和 min 的限制，说明不是合法的bst
+            if (min_node is not None) and (root.val <= min_node.val):
+                return False
+            if (max_node is not None) and (root.val >= max_node.val):
+                return False
+            # 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+            return traverse(root.left, min_node, root) and traverse(root.right, root, max_node)
+
+        return traverse(root, None, None)
+
+
 if __name__ == '__main__':
     S = Solution()
     root = TreeNode(5)
