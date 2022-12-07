b3fe2ca26a91c611d8db53f0137cb96b16f6c5a1
myf-algorithm <1332046108@qq.com>
2022-12-07 17:06:13 +0800

    leetcode

--- Leetcode/239.滑动窗口最大值.py
+++ Leetcode/239.滑动窗口最大值.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
         """
@@ -21,4 +22,45 @@ class Solution(object):
         return res[k - 1:]  # k-1前不是有效的滑动窗口
 
 
+import heapq
 
+
+class Solution_heapq:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        # 注意 Python 默认的优先队列是小根堆
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)  # 将列表转化为堆
+
+        ans = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i - k:  # 最大值索引位于窗口左侧
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+
+        return ans
+
+
+import collections
+
+
+class Solution_deque:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = collections.deque()
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+
+        return ans
