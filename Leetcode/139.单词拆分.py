8ba37b233f16d5245e25f810850564e3a81b0e15
myf-algorithm <1332046108@qq.com>
2022-12-15 11:38:00 +0800

    leetcode

--- Leetcode/139.单词拆分.py
+++ Leetcode/139.单词拆分.py
@@ -1,48 +1,10 @@
 class Solution(object):
     def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        动态规划：
-        状态的定义为：以 s[i] 结尾的子字符串是否可以被空格拆分为一个或多个在字典中出现的单词。
-        """
-        size = len(s)
-        assert size > 0
-        word_set = {word for word in wordDict}  # 把wordDict放进一个哈希表中
-        dp = [False for _ in range(size)]  # 这种状态定义很常见
-        dp[0] = s[0] in word_set
-
-        for r in range(1, size):  # r表示右边界
-            if s[:r + 1] in word_set:
-                dp[r] = True
-                continue
-            for l in range(r):  # l表示左边界
-                if dp[l] and s[l + 1: r + 1] in word_set:
-                    dp[r] = True  # 一旦得到dp[r] = True，break
-                    break
-        return dp[-1]
-
-    def wordBreak1(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        动态规划：
-        状态：dp[i]表示子串s[0:i]（即长度为i的子串，其实就是前缀）可以被空格拆分，
-            并且拆分以后的单词是否落在 wordDict 中。这里 wordDict 要把它放入哈希表中，
-            以快速判断一个单词是否在这个哈希表里。
-        """
-        size = len(s)
-        assert size > 0
-        word_set = {word for word in wordDict} # 预处理，把wordDict放进一个哈希表中
-        # 状态定义：长度为 i 的子串可以被空格拆分为一个或多个在字典中出现的单词
-        dp = [False for _ in range(size + 1)]
+        n = len(s)
+        dp = [False] * (n + 1)  # 表示s的前i位能否用worddict中的单词表示
         dp[0] = True
-        for r in range(1, size + 1):
-            for l in range(r):
-                # dp[l] 写在前面会更快一点，否则还要去切片，然后再放入 hash 表判重
-                if dp[l] and s[l: r] in word_set:
-                    dp[r] = True
-                    break
+        for i in range(n):  # i表示开始索引
+            for j in range(i + 1, n + 1):  # j表示结束索引
+                if (dp[i] and (s[i:j] in wordDict)):
+                    dp[j] = True
         return dp[-1]
--- Leetcode/72.编辑距离.py
+++ Leetcode/72.编辑距离.py
@@ -2,12 +2,15 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n1 = len(word1)
         n2 = len(word2)
-        # (n1 + 1)ĞĞ£¬(n2 + 1)ÁĞ
+
+        # dp[i][j] ä»£è¡¨ word1 åˆ° i ä½ç½®è½¬æ¢æˆ word2 åˆ° j ä½ç½®éœ€è¦æœ€å°‘æ­¥æ•°
+        # dp[i-1][j-1] è¡¨ç¤ºæ›¿æ¢æ“ä½œ
+        # dp[i-1][j] è¡¨ç¤ºåˆ é™¤æ“ä½œ
+        # dp[i][j-1] è¡¨ç¤ºæ’å…¥æ“ä½œ
         dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-        # µÚÒ»ĞĞ
+
         for j in range(1, n2 + 1):
             dp[0][j] = dp[0][j - 1] + 1
-        # µÚÒ»ÁĞ
         for i in range(1, n1 + 1):
             dp[i][0] = dp[i - 1][0] + 1
         for i in range(1, n1 + 1):
@@ -15,34 +18,5 @@ class Solution:
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    # word1 Îª "horse"£¬word2 Îª "ros"
-                    # dp[i - 1][j - 1]µ½dp[i][j]ĞèÒªÌæ»»²Ù×÷
-                    # dp[i - 1][j]µ½dp[i][j]ĞèÒª±íÊ¾É¾³ı²Ù×÷
-                    # dp[i][j - 1]µ½dp[i][j]ĞèÒª±íÊ¾²åÈë²Ù×÷
                     dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
-        return dp[-1][-1]
-
-
-class Solution1:
-    def minEditCost(self, str1, str2, ic, dc, rc):
-        n1 = len(str1)
-        n2 = len(str2)
-        # (n1 + 1)ĞĞ£¬(n2 + 1)ÁĞ
-        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-        # µÚÒ»ĞĞ
-        for j in range(1, n2 + 1):
-            dp[0][j] = dp[0][j - 1] + ic
-        # µÚÒ»ÁĞ
-        for i in range(1, n1 + 1):
-            dp[i][0] = dp[i - 1][0] + dc
-        for i in range(1, n1 + 1):
-            for j in range(1, n2 + 1):
-                if str1[i - 1] == str2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
-                else:
-                    # word1 Îª "horse"£¬word2 Îª "ros"
-                    # dp[i - 1][j - 1]µ½dp[i][j]ĞèÒªÌæ»»²Ù×÷
-                    # dp[i - 1][j]µ½dp[i][j]ĞèÒª±íÊ¾É¾³ı²Ù×÷
-                    # dp[i][j - 1]µ½dp[i][j]ĞèÒª±íÊ¾²åÈë²Ù×÷
-                    dp[i][j] = min(dp[i][j - 1] + ic, dp[i - 1][j] + dc, dp[i - 1][j - 1] + rc)
         return dp[-1][-1]
