497e50be2a306c8ba851bbb1a71e83605912b10a
myf-algorithm <1332046108@qq.com>
2022-12-19 17:19:17 +0800

    leetcode

--- Leetcode/518.零钱兑换II.py
+++ Leetcode/518.零钱兑换II.py
@@ -14,3 +14,16 @@ class Solution:
                     dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
 
         return dp[n][amount]
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                if j >= coins[i - 1]:
+                    dp[j] = dp[j] + dp[j - coins[i - 1]]
+
+        return dp[amount]
