59a8d7799fc984d2c47addeedfb342e9b8181cd4
myf-algorithm <1332046108@qq.com>
2023-01-30 16:29:40 +0800

    lc2

--- Leetcode/140.单词拆分 II.py
+++ Leetcode/140.单词拆分 II.py
@@ -15,27 +15,3 @@ class Solution:
 
         backtracking(s, [])
         return res
-
-
-class Solution1:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        wordDict2Set = set(wordDict)
-        import collections
-        dic = collections.defaultdict(list)
-
-        def backtracking(s):
-            if s in dic:
-                return dic[s]
-            res = []
-            if not s:
-                return [[]]
-            for i in range(len(s)):
-                if s[:i + 1] in wordDict2Set:
-                    cur = backtracking(s[i + 1:])
-                    for tmp in cur:
-                        tmp = [s[:i + 1]] + tmp
-                        res.append(tmp)
-            dic[s] = res
-            return res
-
-        return [' '.join(tmp) for tmp in backtracking(s)]
