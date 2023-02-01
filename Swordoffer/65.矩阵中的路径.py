bca63d430ccb003acfe314880bbaacc91920ad35
myf-algorithm <1332046108@qq.com>
2023-02-01 15:09:51 +0800

    lc2

--- Swordoffer/65.矩阵中的路径.py
+++ Swordoffer/65.矩阵中的路径.py
@@ -10,54 +10,30 @@
 
 
 class Solution:
-    def hasPath(self, matrix, rows, cols, path):
-        if len(matrix) == 0 or len(matrix) != rows * cols or len(path) == 0:
+    def dfs(self, m, i, j, word_idx):
+        if i < 0 or i >= len(m) or j < 0 or j >= len(m[0]) or self.visted[i][j] or m[i][j] != self.word[word_idx]:
+            # 边界值越界 / 已走过 / 值不同
             return False
-        visited = [False] * len(matrix)
-        pathlength = [0]
-        # 以矩阵中每一个位置作为起点进行搜索
-        for i in range(rows):
-            for j in range(cols):
-                if self.haspath(matrix, rows, cols, path, j, i, visited, pathlength):
+        if word_idx == len(self.word) - 1:
+            return True
+        self.visted[i][j] = 1
+        # 左下右上的顺序遍历
+        res = self.dfs(m, i, j - 1, word_idx + 1) or \
+              self.dfs(m, i + 1, j, word_idx + 1) or \
+              self.dfs(m, i, j + 1, word_idx + 1) or \
+              self.dfs(m, i - 1, j, word_idx + 1)
+        self.visted[i][j] = 0
+        return res
+
+    def hasPath(self, matrix, word):
+        self.word = word
+        self.visted = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
+        word_idx = 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if self.dfs(matrix, row, col, word_idx):
                     return True
         return False
-
-    # 判断矩阵位置（x,y）的字符能否加入已找到的路径中
-    def haspath(self, matrix, rows, cols, path, x, y, visited, pathlength):
-        '''
-        :param matrix:字符矩阵
-        :param rows:矩阵的行数
-        :param cols:矩阵的列数
-        :param path:需要寻找的路径
-        :param x:当前位置的横坐标(对应列数)
-        :param y:当前位置的纵坐标(对应行数)
-        :param visited:访问标志数组
-        :param pathlength:已经找到的路径长度
-        :return:是否存在路径
-        '''
-        if pathlength[0] == len(path):
-            return True
-        curhaspath = False
-        # 参数校验：
-        # 1、位置坐标不超过行列数
-        # 2、当前位置字符等于路径中对应位置的字符
-        # 3、当前位置未存在于当前已找到的路径中
-        if 0 <= x < cols \
-                and 0 <= y < rows \
-                and matrix[y * cols + x] == path[pathlength[0]] \
-                and not visited[y * cols + x]:
-            visited[y * cols + x] = True
-            pathlength[0] += 1
-            # 分别向左，向右，向上，向下移动一个格子，任一方向能够继续往下走均可
-            curhaspath = self.haspath(matrix, rows, cols, path, x - 1, y, visited, pathlength) \
-                         or self.haspath(matrix, rows, cols, path, x, y - 1, visited, pathlength) \
-                         or self.haspath(matrix, rows, cols, path, x + 1, y, visited, pathlength) \
-                         or self.haspath(matrix, rows, cols, path, x, y + 1, visited, pathlength)
-            # 如果不能再走下一步，需要回退到上一状态
-            if not curhaspath:
-                pathlength[0] -= 1
-                visited[y * cols + x] = False
-        return curhaspath
 
 
 if __name__ == '__main__':
